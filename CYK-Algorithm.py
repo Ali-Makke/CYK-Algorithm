fd32a99c9e4037841351d538534c4bf628c991bb
Odigras <68686511+Odigras@users.noreply.github.com>
2024-03-20 18:13:03 +0200

    optimize with memo attempt CYK-Algorithm.py
    Added a memo{} dictionary to avoid redundant operations.
    Using a dynamic programming concept called memoization.
    
    NOTE: 
    The code works but the memoization technique did not reduce the execution time of the code for some reason.

--- CYK-Algorithm.py
+++ CYK-Algorithm.py
@@ -1,14 +1,16 @@
 from itertools import product
-#enter your grammar rules here (Must be in Chomsky Normal Form(CNF))
+
+# enter your grammar rules here (Must be in Chomsky Normal Form(CNF))
 Rules = {
     "S": [("A", "B"), ("B", "C")],
     "A": [("B", "A"), "a"],
     "B": [("C", "C"), "b"],
     "C": [("A", "B"), "a"]
 }
-#input the starting non-terminal here:
+
+# input the starting non-terminal here:
 start = "S"
-#input word here
+# input word here
 word = "b a a b a".split()
 
 wordLen = len(word)
@@ -24,27 +26,34 @@ for letter in word:
     i += 1
 
 
-def isInGrammar(letterList, l, k1):
-    for tupl in letterList:
-        for key0 in Rules:
-            if tupl in Rules[key0]:
-                chart[l + 1][k1].append(key0)
+def isInGrammar(list_Product, level, k):
+    for tupl in list_Product:
+        for key in Rules:
+            if tupl in Rules[key]:
+                chart[level + 1][k].append(key)
 
 
+memo = {}
 # helps the inner loops(traverser the chart all in all)
 level = -1
 for j in range(wordLen, -1, -1):
     level += 1
-    # iterates through the rows from longest row(first row) to shortest row(last row)
-    # moves to the next row for each outer loop(for each j)
+    # iterates through the cells from left to right
+    # selects the next cell for each outer loop(for each j)
     for k in range(j - 1):
+        # Gets all relevant combinations of previous relevant cells
         for p in range(level + 1):
+            if all(not sub_list for sub_list in chart[p][k]) or all(
+                    not sub_list for sub_list in chart[level - p][p + k + 1]):
+                continue
             list1, list2 = chart[p][k], chart[level - p][p + k + 1]
-            isInGrammar(list(product(list1, list2)), level, k)
+            list_Product = tuple(product(list1, list2))
+            if memo.get(list_Product) is not None:
+                chart[level + 1][k].append(memo[list_Product])
+                continue
+            isInGrammar(list_Product, level, k)
+            memo.update({list_Product: ', '.join(chart[level + 1][k])})
 
-#prints true if the word is valid in your grammar
-print(start in chart[wordLen - 1][0], "-> your word is valid for this grammar")
-
-#shows the chart after filled:
-#for row in chart:
-#    print(row)
+# prints true if the word is valid in your grammar
+print(start in chart[wordLen - 1][0] or start in [item.strip() for item in chart[wordLen - 1][0][1].split(',') if
+                                                  item.strip()])
